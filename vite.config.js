f918b0b776ae6f1972d71021b1bfc66bdb8b7f08
Niek de Gier <n.de.gier@hotmail.com>
2025-03-25 12:00:12 +0100

    Deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/WebsiteMayra/', // This should match your repository name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  }
 })
