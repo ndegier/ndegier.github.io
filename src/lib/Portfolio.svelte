cfdddad4a5e9dc1ff5239c773878b672906be148
Niek de Gier <n.de.gier@hotmail.com>
2025-04-06 20:41:00 +0200

    Image paths

--- src/lib/Portfolio.svelte
+++ src/lib/Portfolio.svelte
@@ -3,7 +3,7 @@
 
   const items = portfolio.map(item => {
     return {
-      "image": `src/assets/Portfolio/${item.image}`,
+      "image": `../assets/${item.image}`,
       "title": item.title,
       "description": item.description
     }
