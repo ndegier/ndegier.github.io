5325c3863c91a388b6ac9b9d3afcf93c88ad3905
Niek de Gier <n.de.gier@hotmail.com>
2025-04-06 11:19:47 +0200

    Insta embed (not yet added to body)

---
+++ src/lib/InstaEmbed.svelte
@@ -0,0 +1,20 @@
+<!-- Embed.svelte -->
+<script>
+    import { onMount } from 'svelte';
+  
+    onMount(() => {
+      const script = document.createElement('script');
+      script.setAttribute('src', 'https://www.instagram.com/embed.js');
+      script.async = true;
+      document.body.appendChild(script);
+    });
+  </script>
+  
+  <div>
+    <blockquote 
+      class="instagram-media" 
+      data-instgrm-permalink="https://www.instagram.com/mayraskleipraatjes/"
+      data-instgrm-version="14"
+      style="background:#FFF; border:0; margin: 1rem 0; padding:0;">
+    </blockquote>
+  </div>
